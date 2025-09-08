15f2ce053c5094fd6b141b5df1c9b4f3b374d4c9
Daniel2024212 <luisdanielanayatorres@gmail.com>
2025-09-08 11:16:06 -0600

    Cambios 10

--- models/Cita.php
+++ models/Cita.php
@@ -6,17 +6,17 @@ class Cita extends ActiveRecord {
 
     // Base de datos:
     protected static $tabla = 'citas';
-    protected static $columnasDB = ['id', 'fecha', 'hora', 'nombre_cliente'];
+    protected static $columnasDB = ['id', 'fecha', 'hora', 'usuarioId'];
 
     public $id;
     public $fecha;
     public $hora;
-    public $nombre_cliente;
+    public $usuarioId;
 
     public function __construct($args = []) {
         $this->id = $args['id'] ?? null;
         $this->fecha = $args['fecha'] ?? '';
         $this->hora = $args['hora'] ?? '';
-        $this->nombre_cliente = $args['nombre_cliente'] ?? '';
+        $this->usuarioId = $args['usuarioId'] ?? '';
     }
 }
