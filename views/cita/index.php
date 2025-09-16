1d0e0e117163fa6c7ffe568c7becaeee0a05d479
Daniel2024212 <luisdanielanayatorres@gmail.com>
2025-09-15 19:09:55 -0600

    Cambios 104

--- views/cita/index.php
+++ views/cita/index.php
@@ -26,7 +26,7 @@ include_once __DIR__ . '/../templates/barra.php';
         <form class="formulario">
             <div class="campo">
                 <label for="nombre">Nombre</label>
-                <input id="nombre" type="text" placeholder="Tu Nombre">
+                <input id="nombre" type="text" placeholder="Tu Nombre" value="<?php echo s($nombre); ?>" disabled>
             </div> <!-- .campo -->
 
             <div class="campo">
