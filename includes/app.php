4253d68f31cfd5484d6323bb385f810f3d6f8400
Daniel2024212 <luisdanielanayatorres@gmail.com>
2025-12-02 08:19:37 -0600

    Prueba208

--- includes/app.php
+++ includes/app.php
@@ -4,17 +4,6 @@ use Dotenv\Dotenv;
 use Models\ActiveRecord;
 
 require __DIR__ . '/../vendor/autoload.php';
-
-// --- SOLUCIÓN ERROR 500: CARGA MANUAL DE SERVICIOS ---
-// Agregamos esto porque el autoloader del servidor no se ha actualizado
-require_once __DIR__ . '/../classes/Email.php';
-require_once __DIR__ . '/../classes/AuthService.php';
-require_once __DIR__ . '/../classes/CatalogoService.php';
-require_once __DIR__ . '/../classes/CitaService.php';
-require_once __DIR__ . '/../classes/PagoService.php';
-require_once __DIR__ . '/../classes/ValoracionService.php';
-// -----------------------------------------------------
-
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
--- models/AdminCita.php
+++ models/AdminCita.php
@@ -3,23 +3,14 @@
 namespace Models;
 
 class AdminCita extends ActiveRecord {
-    protected static $tabla = 'citasServicios';
-    // AGREGAMOS 'fecha' AQUÍ
-    protected static $columnasDB = ['id', 'fecha', 'hora', 'cliente', 'email', 'telefono', 'servicio', 'precio'];
 
-    // Y AQUÍ TAMBIÉN
-    public $id;
-    public $fecha; 
-    public $hora;
-    public $cliente;
-    public $email;
-    public $telefono;
-    public $servicio;
-    public $precio;
+    protected static $tabla = 'citasServicios';
+    protected static $columnasDB = ['id', 'hora', 'cliente', 'email', 'telefono', 'servicio', 'precio'];
+
+    public $id, $hora, $cliente, $email, $telefono, $servicio, $precio;
 
     public function __construct($args =[]) {
         $this->id = $args['id'] ?? null;
-        $this->fecha = $args['fecha'] ?? ''; // Inicializar
         $this->hora = $args['hora'] ?? '';
         $this->cliente = $args['cliente'] ?? '';
         $this->email = $args['email'] ?? '';
