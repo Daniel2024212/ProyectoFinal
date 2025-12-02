1cbb1d68b2936efce1acaf289bbabd490e044561
Daniel2024212 <luisdanielanayatorres@gmail.com>
2025-12-02 07:48:05 -0600

    Prueba205

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -2,18 +2,16 @@
 
 namespace Controllers;
 
+use Models\AdminCita;
 use MVC\Router;
-use Classes\CitaService; 
+use Classes\CitaService;
 
 class AdminController {
 
     public static function index(Router $router) {
-        session_start();
-
-        // Verificaciones de acceso (Middleware)
-        if(!isset($_SESSION['admin'])) {
-            header('Location: /');
-        }
+        isSession();
+        isAuth();
+        isAdmin();
 
         $fecha = $_GET['fecha'] ?? date('Y-m-d');
         $fechas = explode('-', $fecha);
@@ -22,13 +20,27 @@ class AdminController {
             header('Location: /404');
         }
 
-        // CORRECCIÓN: El método se llama obtenerAgendaPorFecha en el servicio
-        $citas = CitaService::obtenerAgendaPorFecha($fecha);
+        $nombre = $_SESSION['nombre'];
+        
+        // Consultar la BD:
+        $consulta = "SELECT citas.id, citas.hora, CONCAT( usuarios.nombre, ' ', usuarios.apellido) as cliente, ";
+        $consulta .= " usuarios.email, usuarios.telefono, servicios.nombre as servicio, servicios.precio  ";
+        $consulta .= " FROM citas  ";
+        $consulta .= " LEFT OUTER JOIN usuarios ";
+        $consulta .= " ON citas.usuarioId=usuarios.id  ";
+        $consulta .= " LEFT OUTER JOIN citasServicios ";
+        $consulta .= " ON citasServicios.citaId=citas.id ";
+        $consulta .= " LEFT OUTER JOIN servicios ";
+        $consulta .= " ON servicios.id=citasServicios.servicioId ";
+        $consulta .= " WHERE fecha =  '{$fecha}' ";
+
+        $citas = AdminCita::SQL($consulta);
         
         $router->render('admin/index', [
-            'nombre' => $_SESSION['nombre'],
+            'nombre' => $nombre,
             'citas' => $citas,
             'fecha' => $fecha
         ]);
     }
+    
 }
