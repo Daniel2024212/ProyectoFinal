6cd80aa5876f2e0412946a3cb47b08131bbcc4d3
Daniel2024212 <luisdanielanayatorres@gmail.com>
2025-12-10 18:52:45 -0600

    Pru11

--- views/layout.php
+++ views/layout.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>App Salón</title>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
     <link rel="stylesheet" href="/build/css/app.css">
 </head>
+
 <body>
     <div class="contenedor-app">
         <div class="imagen"></div>
@@ -17,13 +19,16 @@
     </div> <!-- .contenedor-app -->
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script src="/build/js/app.js"></script>
     <?php
-        echo $script ?? '';
+    echo $script ?? '';
     ?>
 
     <?php
-        echo $script ?? '';
+    echo $script ?? '';
     ?>
 
 </body>
+
 </html>
